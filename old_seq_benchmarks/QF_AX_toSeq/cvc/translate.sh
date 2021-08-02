afae9c3424ed83729bab0b458f9dd050018090b4
Ying1123 <sqy1415@gmail.com>
2021-08-02 12:07:30 -0700

    unimportant

--- old_seq_benchmarks/QF_AX_toSeq/cvc/translate.sh
+++
@@ -1,5 +0,0 @@
-sed -i "s/select/seq.nth/" read5.smt2
-sed -i "s/store/seq.update/" read5.smt2
-sed -i "s/set-logic.*/set-logic ALL)/" read5.smt2
-sed -i "s/(Array Int /(Seq /" read5.smt2
-
--- old_seq_benchmarks/QF_AX_toSeq/translate.sh
+++
@@ -1,4 +0,0 @@
-sed -i "s/select/seq.nth/" storeinv_t1_np_nf_ai_00002_001.cvc.smt2
-sed -i "s/store/seq.update/" storeinv_t1_np_nf_ai_00002_001.cvc.smt2
-sed -i "s/set-logic.*/set-logic ALL)/" storeinv_t1_np_nf_ai_00002_001.cvc.smt2
-sed -i "s/(Array Index /(Seq /" storeinv_t1_np_nf_ai_00002_001.cvc.smt2
